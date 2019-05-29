bbdd606fe5e147f2d46c5d9ff4a9802690e34605
unknown <lugowski.k@gmail.com>
2019-05-29 22:39:11 +0200

    Updates

---
+++ precache-manifest.bed7ab5ec51a8a88d219aa59a617faa3.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0953d02aa41be9403580659e1bb610c4",
+    "url": "/okay/index.html"
+  },
+  {
+    "revision": "2cb6cc01a0660b1620c9",
+    "url": "/okay/static/css/main.c76d835c.chunk.css"
+  },
+  {
+    "revision": "7ea99231b87498d328d7",
+    "url": "/okay/static/js/2.13e21f65.chunk.js"
+  },
+  {
+    "revision": "2cb6cc01a0660b1620c9",
+    "url": "/okay/static/js/main.d9975cb0.chunk.js"
+  },
+  {
+    "revision": "8c042b0823ec14d0eb6b",
+    "url": "/okay/static/js/runtime~main.ae7eec07.js"
+  },
+  {
+    "revision": "aed64a27ab824abbd435f82fdefd381c",
+    "url": "/okay/static/media/abr1.aed64a27.jpg"
+  },
+  {
+    "revision": "fde508bc551a7bf4e60861e1ad98bd81",
+    "url": "/okay/static/media/abr2.fde508bc.jpg"
+  },
+  {
+    "revision": "c4d561d16f2f6fe500fcccd01caf19da",
+    "url": "/okay/static/media/abr3.c4d561d1.jpg"
+  },
+  {
+    "revision": "03c2bc792d8b5b7466935c15423958da",
+    "url": "/okay/static/media/abroad.03c2bc79.jpg"
+  },
+  {
+    "revision": "e6cd91bcf48c0a9c66f0c69ee4054724",
+    "url": "/okay/static/media/abroad.e6cd91bc.jpg"
+  },
+  {
+    "revision": "4100372494ef7fc2a43ea12fe0dd868b",
+    "url": "/okay/static/media/b2b1.41003724.jpg"
+  },
+  {
+    "revision": "c045efaa4955de702284d268ee581eac",
+    "url": "/okay/static/media/b2b2.c045efaa.jpg"
+  },
+  {
+    "revision": "e8087415da3480b97753f966773f21cf",
+    "url": "/okay/static/media/b2b3.e8087415.jpg"
+  },
+  {
+    "revision": "44a2fa7ff4cc0e0bb45c9649d85310f3",
+    "url": "/okay/static/media/boy-girl.44a2fa7f.jpg"
+  },
+  {
+    "revision": "9650c6364ab0d5c870ee133ca21a0444",
+    "url": "/okay/static/media/business.9650c636.jpg"
+  },
+  {
+    "revision": "c76cee89f43c6591c5aec4f9e717bca7",
+    "url": "/okay/static/media/flag.c76cee89.jpg"
+  },
+  {
+    "revision": "ae0f06c56ae7df74733e7509306c5bae",
+    "url": "/okay/static/media/ge.ae0f06c5.jpg"
+  },
+  {
+    "revision": "c59a2c716b366253438509f7423513b5",
+    "url": "/okay/static/media/ge1.c59a2c71.jpg"
+  },
+  {
+    "revision": "5bed8a5893c91271a674a96b0be880ed",
+    "url": "/okay/static/media/hands.5bed8a58.jpg"
+  },
+  {
+    "revision": "c433accee55ff9e382ce36d5acb6804d",
+    "url": "/okay/static/media/man.c433acce.jpg"
+  },
+  {
+    "revision": "1835f7eeb399acf55153a54324139a5a",
+    "url": "/okay/static/media/manwitflag.1835f7ee.jpg"
+  },
+  {
+    "revision": "9eb3560533ff1d7868d0ec73aa22f2bb",
+    "url": "/okay/static/media/pencils.9eb35605.mov"
+  },
+  {
+    "revision": "65d4962225a9e778871ecc7d10cf6084",
+    "url": "/okay/static/media/puzzles.65d49622.jpg"
+  },
+  {
+    "revision": "045c8a07346e88efe3f3d7f0d0334bd3",
+    "url": "/okay/static/media/tongue.045c8a07.jpg"
+  },
+  {
+    "revision": "e86ca3746c407bc7d08f01d12cb7c3a6",
+    "url": "/okay/static/media/tube.e86ca374.jpg"
+  },
+  {
+    "revision": "f77efc8e4051abb807fd07a5f2b89e97",
+    "url": "/okay/static/media/uk1.f77efc8e.jpg"
+  },
+  {
+    "revision": "9024b77b4abab4ea93b8e5f9ccd93f60",
+    "url": "/okay/static/media/up.9024b77b.jpg"
+  }
+]);
